edf3ffd8e31b52ece03969501184056b730be917
Kchun510 <kylechun1@gmail.com>
2025-01-21 16:50:20 -0800

    First benchmark commit, branch made to derive a percentage of validation accuracy

--- mediaCheck.ts
+++ mediaCheck.ts
@@ -5,9 +5,8 @@ dotenv.config();
 const apiKey = process.env.gemAPIKey || "";
 const fileManager = new GoogleAIFileManager(apiKey);
 const genAI = new GoogleGenerativeAI(apiKey);
-
 const model = genAI.getGenerativeModel({
-	model: "gemini-1.5-pro",
+	model: "gemini-1.5-flash",
 });
 
 const uploadFile = async (fileName: string) => {
@@ -59,6 +58,8 @@ const evalVideo = async (uploadResponse: any, invalidParams: string) => {
 		},
 	])).response.text()
 
+	console.log(videoAnalysisRes)
+
 	const prompt2 = `Your job is to decide if a parameter was broken based on this text: 
 	${videoAnalysisRes}.
 	Only respond with a single word, If any parameter is broken return "false" , else return "true". 
@@ -71,22 +72,70 @@ const evalVideo = async (uploadResponse: any, invalidParams: string) => {
 }
 
 const paramForBadVideo = `
-1) If anypop-up or add appears.
-2) If visual gameplay is not fast-paced combat or intense physical movement, then the parameters are violated.
-3) If any of the audio is to extreme, i.e: The talk of death.
+1) If there appears to be a video intro, for example theres one video that transitions into the main content, excluding overlayed text.
+2) If the video does not show people (fictional or non-fictional) or has many still frames exluding text.
+3) If a you-tube looking pop-up appears.
+4) If any of the audio is to extreme, i.e: The talk of death.
 `
+function getRandomInt(min: number, max: number) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function select_vid(): { file_name: string, type: string } {
+	const state = getRandomInt(0, 5)
+	switch (state) {
+		case 0:
+			return { file_name: "/home/toastycommand/Downloads/sho3kd-P2001.mp4", type: "Bad" }
+		case 1:
+			return { file_name: "/home/toastycommand/Downloads/1837bm4000.mp4", type: "Good" }
+		case 2:
+			return { file_name: "/home/toastycommand/Downloads/1652hm8000.mp4", type: "Bad" }
+		case 3:
+			return { file_name: "/home/toastycommand/Downloads/1f2nziz000.mp4", type: "Good" }
+		case 4:
+			return { file_name: "/home/toastycommand/Downloads/1h2mg3t000.mp4", type: "Bad" }
+		case 5:
+			return { file_name: "/home/toastycommand/Desktop/content_for_system/test_twitch/prod_vids/video_plus_sub/SmokyImpartialChoughTakeNRG-Vd_4OM_q_eTeSUPQ000.mp4", type: "Good" }
+	}
+	return { file_name: "", type: "null" }
+}
 
 export async function main(input: { filePath?: string, invalidParam?: string } = { filePath: "test.mp4", invalidParam: paramForBadVideo }) {
-
-	const invalidParam = input.invalidParam ?? paramForBadVideo;
-	const uploadResponse = await uploadFile(input.filePath ?? "")
-	await checkFileState(uploadResponse.file.name)
-	console.log(await evalVideo(uploadResponse, invalidParam))
+	let res_arr: object[] = []
+	let correct_count = 0
+	let incorrect_count = 0
+	let test_count = 100
+	for (let i = 0; i < test_count; i++) {
+		const file = select_vid()
+		const invalidParam = input.invalidParam ?? paramForBadVideo;
+		const uploadResponse = await uploadFile(file.file_name)
+		await checkFileState(uploadResponse.file.name)
+		const evalRes = (await evalVideo(uploadResponse, invalidParam)).trim()
+		if (evalRes == "true") {
+			if (file.type == "Good") {
+				correct_count++
+			} else {
+				incorrect_count++
+			}
+		}
+		if (evalRes == "false") {
+			if (file.type == "Bad") {
+				correct_count++
+			} else {
+				incorrect_count++
+			}
+		}
+		console.log(evalRes)
+		res_arr.push({ res: evalRes, truth: file.type })
+	}
+	console.log(res_arr)
+	console.log(`correct: ${correct_count}, incorrect: ${incorrect_count}`)
+	console.log(`Accuracy: ${correct_count / test_count}`)
 }
-const itDramaBad = "/mnt/md0/work_station/content_for_system/ITDrama/prod_vids/single_vid_plus_reddit/sho3kd-P2001.mp4"
-const tellTaleBad = "/mnt/md0/work_station/content_for_system/tellTale/prod_vids/single_vid_plus_reddit/1652hm8000.mp4"
-
-main({ filePath: itDramaBad })
+main()
 
 
 
